3be96fdea2ef8ef4a1802a7b94a60261fdf0031c
nhamonin <nazarii.hamonin@gmail.com>
2024-01-04 11:24:46 +0200

    Change non-active progress color

--- tailwind.config.js
+++ tailwind.config.js
@@ -50,6 +50,10 @@ module.exports = {
           DEFAULT: 'hsl(var(--card))',
           foreground: 'hsl(var(--card-foreground))',
         },
+        progress: {
+          DEFAULT: 'hsl(var(--progress))',
+          foreground: 'hsl(var(--progress-foreground))',
+        },
       },
       borderRadius: {
         lg: 'var(--radius)',
